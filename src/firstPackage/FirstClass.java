3bd0ac3b6eba5f34ebb9a8538e960662a90fb471
Suzan Ozkilic <suzandalgic@gmail.com>
2020-11-07 11:49:18 -0600

    Second Commit

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("It is Saturday. First Commit from local master.");
-
+		System.out.println("Second line of code.Second commit!");
 	}
 
 }
